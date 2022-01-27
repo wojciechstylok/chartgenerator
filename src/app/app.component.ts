207362cba73739f24c3254ddfa3fd24ab9dafa43
Wojtek <oitad@outlook.com>
2022-01-27 13:39:33 +0100

    Added entering chart data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Dataset } from '../models/Dataset';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public chosenChartType: string = 'pie';
+  public chosenChartType: string = 'bar';
+  public labels: string = '';
+  public labelToAdd: string = '';
+  public datasets: string = '';
+  public datasetToAdd: string = '';
+  public datasetLabelToAdd: string = '';
+
+  private _labels: string[] = [];
+  private _datasets: Dataset[] = [];
+
+  onChartTypeSelect(value: string) {
+    this.chosenChartType = value;
+    console.log(this.chosenChartType);
+  }
+
+  addLabel() {
+    this._labels.push(this.labelToAdd);
+    this.labels += this.labelToAdd;
+    if(this.labels.length > 0) this.labels += ', ';
+
+    this.labelToAdd = '';
+  }
+
+  clearLabels() {
+    this.labelToAdd = '';
+    this.labels = '';
+    this._labels = [];
+  }
+
+  onLabelInput(value: string) {
+    this.labelToAdd = value;
+  }
+
+  onDatasetInput(value: string) {
+    this.datasetToAdd = value;
+  }
+
+  onDatasetLabelInput(value: string) {
+    this.datasetLabelToAdd = value;
+  }
+
+  addDataset() {
+    var dataset: Dataset = new Dataset();
+    dataset.data = this.datasetToAdd.split(";");
+    dataset.label = this.datasetLabelToAdd;
+
+    this._datasets.push(dataset);
+
+    this.datasets += this.datasetToAdd + ' ' + this.datasetLabelToAdd + '\n';
+  }
+
+  clearDatasets() {
+    this.datasets = '';
+    this.datasetToAdd = '';
+    this.datasetLabelToAdd = '';
+    this._datasets = [];
+  }
+
+  // ---------- EXAMPLE CHART THINGS BELOW -----------
 
   public chartType: string = 'bar';
 
---
+++ src/models/Dataset.ts
@@ -0,0 +1,4 @@
+export class Dataset {
+    data!: string[];
+    label!: string;
+}
